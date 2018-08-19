47a988d38ab18162b5e8d59dfb030b838f991bf3
Akshay <akshaycm@hotmail.com>
2018-08-19 23:12:30 +0300

    asd

--- setupmn.sh
+++ setupmn.sh
@@ -118,7 +118,6 @@ clear
 }
 
 function update_config() {
-  sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
   cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
 logintimestamps=1
 maxconnections=256
